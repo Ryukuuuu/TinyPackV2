da86cda5c6aeab756d8ba464a614a212afaed426
Ryukuuuu <104591137+Ryukuuuu@users.noreply.github.com>
2023-05-28 21:37:42 +0100

    Transição de estados feita, mexer no context para ter acesso aos valores do environment

--- src/pt/isec/pa/tinypac/model/fsm/states/EndGame.java
+++ src/pt/isec/pa/tinypac/model/fsm/states/EndGame.java
@@ -6,4 +6,12 @@ import pt.isec.pa.tinypac.model.fsm.GameContext;
 public class EndGame extends GameStateAdapter{
 
     public EndGame(EnvironmentManager environmentManager, GameContext gameContext){super(environmentManager,gameContext);}
+
+    @Override
+    public boolean toWaitingForStart(){
+        setState(GameState.WAITING_FOR_START);
+        return true;
+    }
+
+
 }
